9ec87ea5bef18e3e27a88320a441b343f9dac634
Ferroudj Samy <aa691988@ens.uqam.ca>
2026-05-20 21:28:11 -0400

    create features section component

--- src/app/pages/home.page/home.page.ts
+++ src/app/pages/home.page/home.page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar.component/navbar.component";
 import { HeroComponent } from "../../components/hero.component/hero.component";
+import { FeaturesSectionComponent } from '../../components/features-section.component/features-section.component';
 
 @Component({
   selector: 'app-home.page',
-  imports: [NavbarComponent, HeroComponent],
+  imports: [NavbarComponent, HeroComponent, FeaturesSectionComponent],
   templateUrl: './home.page.html',
   styleUrl: './home.page.scss',
 })
